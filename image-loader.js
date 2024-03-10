47722cb5ca77c8bc9731939e9708793ec4a6e066
Niklas Buhl <hello@niklasbuhl.com>
2024-03-10 11:35:54 +0100

    no env

--- image-loader.js
+++ image-loader.js
@@ -1,3 +1,4 @@
 export default function imageLoader({ src }) {
-  return `/nextjs-mdx-learning${src}`
+  const path = process.env.NODE_ENV === 'test' ? '/nextjs-mdx-learning' : ''
+  return path + `${src}`
 }
